f1d36f688b5eb6c065ca081b930296083b391a28
utsav-blaze <47816677+utsav-blaze@users.noreply.github.com>
2021-06-01 13:59:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var x=(Math.floor((Math.random()*6)+1));
+console.log(x);
+var y=(Math.floor((Math.random()*6)+1));
+console.log(y);
+document.querySelector("img.img1").src="images/dice"+x+".png";
+document.querySelector("img.img2").src="images/dice"+y+".png";
+function winner(){
+  if(x>y)
+  document.querySelector("h3").textContent="Player 1 is Winner";
+  else if(y>x)
+  document.querySelector("h3").textContent="Player 2 is Winner";
+  else
+  document.querySelector("h3").textContent="Player 1 is tied with Player 2";
+}
+winner();
